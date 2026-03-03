cc115fc59bd7cdc19ec0d0e36d6c6a9484a9b89d
Rianne Kramer <715731@student.inholland.nl>
2026-03-03 16:21:57 +0100

    Added: - Header - User Modified - App to support header and user components

---
+++ src/app/user/user.ts
@@ -0,0 +1,20 @@
+import { Component, computed, input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => {
+    return 'assets/users/' + this.avatar();
+  });
+
+  onSelectUser() {
+
+  }
+}
